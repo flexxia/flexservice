c67c02ea3cb05ebcf6de5773a9116c64e7e1db02
dongtianzen <dongtianzen@gmail.com>
2019-12-23 20:53:32 -0500

    init ModalPage

---
+++ modalpage/src/Controller/ModalpageController.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\modalpage\Controller\ModalpageController.
+ */
+
+namespace Drupal\modalpage\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Drupal\modalpage\Content\DashpageModalGenerator;
+use Drupal\flexpage\Controller\FlexpageController;
+
+
+/**
+ * An example controller.
+ */
+class ModalpageController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function standardModalPage($section, $entity_id) {
+    $DashpageModalGenerator = new DashpageModalGenerator();
+    $markup = $DashpageModalGenerator->standardModalPage($section, $entity_id);
+
+    dpm($markup['all']['header']);
+    // $markup = render($markup['speaker']);
+    // $markup = render($markup['all']['header']);
+    $markup = render($markup['ytd']['header']);
+
+    $build = array(
+      '#type' => 'markup',
+      '#header' => 'header',
+      '#markup' => $markup,
+      '#allowed_tags' => \Drupal::getContainer()->get('flexinfo.setting.service')->adminTag(),
+    );
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function standardModalJson($section, $entity_id) {
+    $DashpageModalGenerator = new DashpageModalGenerator();
+    $object_content_data['content'] = $DashpageModalGenerator->standardModalPage($section, $entity_id);
+
+    return new JsonResponse($object_content_data);
+
+    // debug output as JSON format
+    $build = array(
+      '#type' => 'markup',
+      '#markup' => json_encode($object_content_data),
+    );
+
+    return $build;
+  }
+
+}
