489b0d1b9b00fc5aa80232c3317a061a8e18dfbd
dongtianzen <dongtianzen@gmail.com>
2020-01-20 07:08:10 -0500

    Update EventtypeFilterForm.php

--- ngpage/src/Form/EventtypeFilterForm.php
+++ ngpage/src/Form/EventtypeFilterForm.php
@@ -30,7 +30,7 @@ class EventtypeFilterForm extends FormBase {
       '#type' => 'checkboxes',
       '#options' => $this->_getTermOptions(),
       '#default_value' => $this->_getUserDefaultValue(),
-      '#title' => '',
+      '#title' => 'EventType Form',
       '#attributes' => array('class' => array('display-inline-block', 'float-left', 'margin-left-24')),
     );
 
@@ -88,6 +88,8 @@ class EventtypeFilterForm extends FormBase {
    * @return
    */
   public function _getTermOptions() {
+    $output = [];
+
     $terms = \Drupal::getContainer()
       ->get('flexinfo.term.service')
       ->getFullTermsFromVidName('eventtype');
@@ -98,7 +100,7 @@ class EventtypeFilterForm extends FormBase {
           $row .= $term->getName();
         $row .= '</span>';
       $row .= '</li>';
-      $options[$term->id()] = $row;
+      $output[$term->id()] = $row;
     }
 
     return $output;
