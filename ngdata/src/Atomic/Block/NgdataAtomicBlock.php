765e62e9aa794fbdaf63140cfcbcefb5fad81992
dongtianzen <dongtianzen@gmail.com>
2020-09-09 10:41:06 -0400

    Update NgdataAtomicBlock.php

--- ngdata/src/Atomic/Block/NgdataAtomicBlock.php
+++ ngdata/src/Atomic/Block/NgdataAtomicBlock.php
@@ -1027,6 +1027,29 @@ class NgdataAtomicBlock extends NgdataAtomic {
   /**
    *
    */
+  public function getBlockHtmlTableByRadioQuestionMultipleByReferTid($question_term = NULL, $meeting_nodes = array()) {
+    $output = array();
+    $table_body = NULL;
+
+    $output = $this->organism->basicSection("htmlSnippt", "float-right margin-bottom-n-24 margin-right-16");
+    $table_body = $this->organism->getHtmlTableByMultipleQuestionByReferTid($question_term, $meeting_nodes);
+
+    if (isset($table_body) && count($table_body) > 0) {
+      // $output['blockIcon'] = '';
+      $output['blockClass'] = "col-xs-12 margin-top-12 min-height-100";
+      $output['blockHeader'] = $this->organism->blockHeaderHtmlQuestionTitle($question_term);
+      $output['blockContent'][0]['tabData']['top']['value'] = $table_body;
+    }
+    else {
+      $output['blockClass'] = "col-xs-12 margin-top-4 block-comment-wrapper";
+    }
+
+    return $output;
+  }
+
+  /**
+   *
+   */
   public function getBlockHtmlTableByRadioQuestionMultipleByReferUid($question_term = NULL, $meeting_nodes = array()) {
     $output = array();
     $table_body = NULL;
