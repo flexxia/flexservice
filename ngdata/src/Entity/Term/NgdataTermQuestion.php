b4aa7207dadd676015b743730db4f173b5c64fc5
Dong Tian <dongtianzen@users.noreply.github.com>
2021-08-31 10:09:04 -0400

    Update NgdataTermQuestion.php

--- ngdata/src/Entity/Term/NgdataTermQuestion.php
+++ ngdata/src/Entity/Term/NgdataTermQuestion.php
@@ -21,12 +21,10 @@ class NgdataTermQuestion extends NgdataTerm {
     $output = '';
 
     // sort the legend by ascent order
-    $question_chartlegend_term = \Drupal::getContainer()
-      ->get('flexinfo.field.service')
+    $question_chartlegend_term = \Drupal::service('flexinfo.field.service')
       ->getFieldFirstTargetIdTermEntity($question_term, $field_name = 'field_queslibr_chartlegend');
 
-    $output = \Drupal::getContainer()
-      ->get('flexinfo.field.service')
+    $output = \Drupal::service('flexinfo.field.service')
       ->getFieldFirstValue($question_chartlegend_term, $field_name = 'field_chartlegend_sortorder');
 
     return $output;
@@ -38,8 +36,7 @@ class NgdataTermQuestion extends NgdataTerm {
   public function getQuestionAnswerAllData($meeting_nodes = array(), $question_tid = NULL) {
     $output = [];
 
-    $evaluation_nodes = \Drupal::getContainer()
-      ->get('baseinfo.querynode.service')
+    $evaluation_nodes = \Drupal::service('baseinfo.querynode.service')
       ->wrapperEvaluationNodeFromMeetingNodes($meeting_nodes);
 
     if ($evaluation_nodes && is_array($evaluation_nodes)) {
@@ -60,20 +57,73 @@ class NgdataTermQuestion extends NgdataTerm {
   /**
    *
    */
+  public function getQuestionAnswerAllDataWithEvaluationNid($meeting_nodes = array(), $question_tid = NULL) {
+    $output = [];
+
+    $evaluation_nodes = \Drupal::service('baseinfo.querynode.service')
+      ->wrapperEvaluationNodeFromMeetingNodes($meeting_nodes);
+
+    if ($evaluation_nodes && is_array($evaluation_nodes)) {
+      foreach ($evaluation_nodes as $evaluation_node) {
+        $result = $evaluation_node->get('field_evaluation_reactset')->getValue();
+
+        foreach ($result as $row) {
+          if ($row['question_tid'] == $question_tid && $row['question_answer']) {
+            $answer = [
+              'evaluation_nid' => $evaluation_node->id(),
+              'question_answer' => $row['question_answer'],
+            ];
+            $output[] = $answer;
+          }
+        }
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   *
+   */
+  public function getQuestionAnswerAllDataWithReferUidWithEvaluationNid($meeting_nodes = array(), $question_tid = NULL) {
+    $evaluation_nodes = \Drupal::service('baseinfo.querynode.service')
+      ->wrapperEvaluationNodeFromMeetingNodes($meeting_nodes);
+
+    $output = array();
+    if ($evaluation_nodes && is_array($evaluation_nodes)) {
+      foreach ($evaluation_nodes as $evaluation_node) {
+        $result = $evaluation_node->get('field_evaluation_reactset')->getValue();
+
+        foreach ($result as $row) {
+          if ($row['question_tid'] == $question_tid && $row['question_answer']) {
+            $answer = [
+              'evaluation_nid' => $evaluation_node->id(),
+              'question_answer' => $row['question_answer'],
+              'refer_uid' => $row['refer_uid'],
+            ];
+            $output[$row['refer_uid']][] = $answer;
+          }
+        }
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   *
+   */
   public function getRaidoQuestionColors($question_term = NULL, $pound_sign = FALSE) {
-    $color_palette = \Drupal::getContainer()
-      ->get('flexinfo.field.service')
+    $color_palette = \Drupal::service('flexinfo.field.service')
       ->getFieldFirstTargetIdTermName($question_term, 'field_queslibr_palette');
 
     if (!$color_palette) {
       $color_palette = 'EventPie5 Reverse';
     }
-    $output = \Drupal::getContainer()
-      ->get('flexinfo.setting.service')
+    $output = \Drupal::service('flexinfo.setting.service')
       ->colorPlateOutputKeyPlusOneByPaletteName($color_palette, $color_key = NULL, $pound_sign, 'f6f6f6');
 
-    $question_scale = \Drupal::getContainer()
-      ->get('flexinfo.field.service')
+    $question_scale = \Drupal::service('flexinfo.field.service')
       ->getFieldFirstValue($question_term, 'field_queslibr_scale');
 
     // only output length is equal with question_scale
@@ -150,8 +200,7 @@ class NgdataTermQuestion extends NgdataTerm {
       ->getRaidoQuestionData($question_term, $meeting_nodes);
     $question_data = array_reverse($question_data);
 
-    $output = \Drupal::getContainer()
-      ->get('flexinfo.calc.service')
+    $output = \Drupal::service('flexinfo.calc.service')
       ->arrayAverageByCount($question_data, 1);
 
     return $output;
@@ -195,8 +244,7 @@ class NgdataTermQuestion extends NgdataTerm {
    *
    */
   public function getSelectkeyQuestionData($question_term = NULL, $meeting_nodes = array()) {
-    $all_answer_terms = \Drupal::getContainer()
-      ->get('flexinfo.field.service')
+    $all_answer_terms = \Drupal::service('flexinfo.field.service')
       ->getFieldAllTargetIdsEntitys($question_term, 'field_queslibr_selectkeyanswer');
 
     $result = $this->getQuestionAnswerAllData($meeting_nodes, $question_term->id());
@@ -218,8 +266,7 @@ class NgdataTermQuestion extends NgdataTerm {
   public function getTextfieldQuestionAllData($meeting_nodes = array(), $question_tid = NULL) {
     $output = array();
 
-    $evaluation_nodes = \Drupal::getContainer()
-      ->get('baseinfo.querynode.service')
+    $evaluation_nodes = \Drupal::service('baseinfo.querynode.service')
       ->wrapperEvaluationNodeFromMeetingNodes($meeting_nodes);
 
     $output = $this->getTextfieldQuestionAllDataFromEvaluationNodes($evaluation_nodes, $question_tid);
