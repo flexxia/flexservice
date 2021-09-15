1b127d5f70be7c26f407f4eaadb1980e4a9f2a7f
Dong Tian <dongtianzen@users.noreply.github.com>
2021-09-15 15:17:31 -0400

    Update FactoryCommentBlockData.php

--- genpdf/src/Content/FactoryCommentBlockData.php
+++ genpdf/src/Content/FactoryCommentBlockData.php
@@ -10,142 +10,35 @@ namespace Drupal\genpdf\Content;
  */
 class FactoryCommentBlockData {
 
-	// row height
-  public $titleRowHeight = 38;
-	public $eachCommentRowHeight = 26;
+  // row height
+  private $titleRowHeight = 38;
+  private $eachCommentRowHeight = 26;
 
-  public $commentFontSize = 12;
+  private $commentFontSize = 12;
 
-  public $frameSizeCol12 = 1280;
+  private $frameSizeCol12 = 1280;
 
   // content position
-  public $contentXPositionCol12 = 128;
-  public $contentYFirstRowPositionCol12 = 72;
+  private $contentXPositionCol12 = 128;
+  private $contentYFirstRowPositionCol12 = 72;
 
   // frame position
-  public $frameLeftXPosition = 110 + 12;
-  public $totalPageHeight = 676;
+  private $frameLeftXPosition = 110 + 12;
+  private $totalPageHeight = 676;
 
   // frame bg color
-  public $headerFillColorR = 0;
-  public $headerFillColorG = 157;
-  public $headerFillColorB = 223;
+  private $headerFillColorR = 0;
+  private $headerFillColorG = 157;
+  private $headerFillColorB = 223;
 
   // frame text color
-  public $textColorR = 255;
-  public $textColorG = 255;
-  public $textColorB = 255;
+  private $textColorR = 255;
+  private $textColorG = 255;
+  private $textColorB = 255;
 
-  function getHeaderFillColorR() {
-    return $this->headerFillColorR;
-  }
-
-  function setHeaderFillColorR($parameter) {
-    $this->headerFillColorR = $parameter;
-  }
-
-  function getHeaderFillColorG() {
-    return $this->headerFillColorG;
-  }
-
-  function setHeaderFillColorG($parameter) {
-    $this->headerFillColorG = $parameter;
-  }
-
-  function getHeaderFillColorB() {
-    return $this->headerFillColorB;
-  }
-
-  function setHeaderFillColorB($parameter) {
-    $this->headerFillColorB = $parameter;
-  }
-
-  function getTextColorR() {
-    return $this->textColorR;
-  }
-
-  function setTextColorR($parameter) {
-    $this->textColorR = $parameter;
-  }
-
-  function getTextColorG() {
-    return $this->textColorG;
-  }
-
-  function setTextColorG($parameter) {
-    $this->textColorG = $parameter;
-  }
-
-  function getTextColorB() {
-    return $this->textColorB;
-  }
-
-  function setTextColorB($parameter) {
-    $this->textColorB = $parameter;
-  }
-
-  function getTitleRowHeight() {
-    return $this->titleRowHeight;
-  }
-
-  function setTitleRowHeight($parameter) {
-    $this->titleRowHeight = $parameter;
-  }
-
-  function getEachCommentRowHeight() {
-    return $this->eachCommentRowHeight;
-  }
-
-  function setEachCommentRowHeight($parameter) {
-    $this->eachCommentRowHeight = $parameter;
-  }
-
-  function getCommentFontSize() {
-    return $this->commentFontSize;
-  }
-
-  function setCommentFontSize($parameter) {
-    $this->commentFontSize = $parameter;
-  }
-
-  function getFrameSizeCol12() {
-    return $this->frameSizeCol12;
-  }
-
-  function setFrameSizeCol12($parameter) {
-    $this->frameSizeCol12 = $parameter;
-  }
-
-  function getContentXPositionCol12() {
-    return $this->contentXPositionCol12;
-  }
-
-  function setContentXPositionCol12($parameter) {
-    $this->contentXPositionCol12 = $parameter;
-  }
-
-  function getContentYFirstRowPositionCol12() {
-    return $this->contentYFirstRowPositionCol12;
-  }
-
-  function setContentYFirstRowPositionCol12($parameter) {
-    $this->contentYFirstRowPositionCol12 = $parameter;
-  }
-
-  function getFrameLeftXPosition() {
-    return $this->frameLeftXPosition;
-  }
-
-  function setFrameLeftXPosition($parameter) {
-    $this->frameLeftXPosition = $parameter;
-  }
-
-  function getTotalPageHeight() {
-    return $this->totalPageHeight;
-  }
-
-  function setTotalPageHeight($parameter) {
-    $this->totalPageHeight = $parameter;
+  // Get data from inaccessible (protected or private) or non-existing properties.
+  public  function __get($name){
+    return $this->{$name};
   }
 
 }
