cebf89b7a4dff416bff110ccb22e44459e8cacfa
Dong Tian <dongtianzen@users.noreply.github.com>
2021-09-21 14:09:53 -0400

    Update NgdataAtomicMolecule.php

--- ngdata/src/Atomic/Molecule/NgdataAtomicMolecule.php
+++ ngdata/src/Atomic/Molecule/NgdataAtomicMolecule.php
@@ -1242,11 +1242,11 @@ class NgdataAtomicMolecule extends NgdataAtomic {
         'City' => \Drupal::service('flexinfo.field.service')->getFieldFirstTargetIdTermName($node, 'field_meeting_city'),
         'Speaker' => \Drupal::service('flexinfo.field.service')->getFieldFirstTargetIdUserName($node, 'field_meeting_speaker'),
         'Num' => \Drupal::service('flexinfo.field.service')->getFieldFirstValue($node, 'field_meeting_evaluationnum'),
+        'Edit' => \Drupal::service('flexinfo.node.service')->getNodeEditLink($node->id()),
+        'Add' => \Drupal::l('Add', Url::fromUserInput('/flexform/node/add/evaluation/form/' . $node->id())),
+        'Summary' => \Drupal::l('Add', Url::fromUserInput('/ngpage/summaryevaluation/basicform/' . $node->id())),
         // \Drupal::l('Add', Url::fromUserInput('/manageinfo/node/evaluation/add/form/' . $node->id())),
         // \Drupal::l('Summary', Url::fromUserInput('/manageinfo/summaryevaluation/form/' . $node->id())),
-        'Edit' => \Drupal::service('flexinfo.node.service')->getNodeEditLink($node->id()),
-        'Add' => \Drupal::l('Add', Url::fromUserInput('/ngpage/evaluation/form/' . $node->id())),
-        'Summary' => \Drupal::l('Add', Url::fromUserInput('/ngpage/summaryevaluation/basicform/' . $node->id())),
       );
 
       $row = $result_row;
