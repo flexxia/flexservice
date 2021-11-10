efed0feafc1e9a7df84380782d8b8754af1e88a4
Dong Tian <dongtianzen@users.noreply.github.com>
2021-11-10 09:48:29 -0500

    Update HtmlpageAtomicMolecule.php

--- htmlpage/src/Atomic/Molecule/HtmlpageAtomicMolecule.php
+++ htmlpage/src/Atomic/Molecule/HtmlpageAtomicMolecule.php
@@ -34,28 +34,4 @@ class HtmlpageAtomicMolecule extends HtmlpageAtomic {
     return $output;
   }
 
-  /**
-   * @param $save_png_icon_style is css style, need include "float-right" and margin-top-12 margin-right-16
-   */
-  public function savePngIcon($save_png_icon_style = NULL, $save_block_id = NULL, $save_png_icon_enable = TRUE) {
-    $output = "";
-    $output .= '<div class="drop-down-icon-wrapper dropdown show ' . $save_png_icon_style . '">';
-
-      if ($save_png_icon_enable) {
-        $output .= '<a class="drop-down-icon-toggle dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
-          $output .= '<i class="fa fa-angle-down color-fff"></i>';
-        $output .= '</a>';
-
-        $output .= '<div class="drop-down-icon-menu dropdown-menu padding-20 margin-left-n-86 text-align-center" aria-labelledby="dropdownMenuLink">';
-          $output .= '<a onclick="saveHtmlToPng(\'' . $save_block_id . '\')" class="dropdown-item color-000 font-size-14" href="javascript:void(0);">';
-            $output .= 'SAVE PNG';
-          $output .= '</a>';
-        $output .= '</div>';
-      }
-
-    $output .= '</div>';
-
-    return $output;
-  }
-
 }
