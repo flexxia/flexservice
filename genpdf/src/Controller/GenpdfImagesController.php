e7062a1af95bc4b10727ff8c8256e8512f8698c0
Dong Tian <dongtianzen@users.noreply.github.com>
2022-01-13 12:16:08 -0500

    Adding Image to PDF file by FPDF

---
+++ genpdf/src/Controller/GenpdfImagesController.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\genpdf\Controller\GenpdfImagesController.
+ */
+
+namespace Drupal\genpdf\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use FPDF;
+
+/**
+ * An example controller.
+ */
+class GenpdfImagesController extends ControllerBase {
+
+  /**
+   *
+   */
+  public function pdfFromImages() {
+    $image_path = drupal_get_path( 'module', 'genpdf') . '/images/';
+
+    $pdf = new FPDF();
+    $pdf->AddPage();
+
+    $pdf->SetXY(28, 10);
+    $pdf->SetFont('Arial', '', 16);
+    $pdf->SetTextColor(50, 178, 228);
+    $pdf->Write(10, 'Hello World!');
+
+    $pdf->Image($image_path . '001.png', 20, 20, 120, 120);
+    $pdf->Image($image_path . '002.png', 20, 160, 120, 120);
+
+    $pdf->AddPage();
+    $pdf->Image($image_path . '003.png', 20, 20, 120, 120);
+
+    $pdf->Output();
+
+
+    $markup = '';
+    $markup .= '<div class="row padding-0">';
+      $markup .= '<div class="text-center">';
+        $markup .= 'PDF File Generate Successful22';
+        $markup .= '<br />';
+        $markup .= $image_path;
+      $markup .= '</div>';
+    $markup .= '</div>';
+
+    $build = array(
+      '#type' => 'markup',
+      '#header' => 'header',
+      '#markup' => $markup,
+    );
+
+    return $build;
+  }
+
+}
