c300caa4cd734711d12e7979567385e749b29440
Dong Tian <dongtianzen@users.noreply.github.com>
2022-02-27 10:29:35 -0500

    render-by-template-form.html.twig

---
+++ ajaxtable/src/Form/DemoFormRenderByTemplateForm.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\ajaxtable\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Render\Element;
+
+/**
+ * Class DemoForm.
+ */
+class DemoFormRenderByTemplateForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'demo_form_clear_action_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['group']['country'] = array(
+      '#type' => 'textfield',
+      '#title' => "Country",
+      '#description' => 'Country Name',
+    );
+    $form['group']['province'] = array(
+      '#type' => 'textfield',
+      '#title' => "Province",
+      '#description' => 'Province Name',
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => 'Submit',
+    );
+
+    /**
+     * passing template name, hook_theme() on the .module file
+     */
+    $form['#theme'] = 'render_by_template_form';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      // @TODO: Validate fields.
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::messenger()->addMessage('Submit callback from submitForm()');
+
+    // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
+    }
+  }
+
+}
+
